05e75f4bf7d8df2b8553ddc09abad3fd57e568d0
peter <peterboshra1000@gmail.com>
2025-05-07 19:11:11 +0300

    Add Second ID : 20230098

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("First ID : 20230099");
+        System.out.println("Second ID : 20230098");
     }
     
 }
